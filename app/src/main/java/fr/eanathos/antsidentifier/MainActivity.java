8d529cae1c2d1dcd1b82c0d3ef9140492c1a4864
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-02-23 11:19:03 +0100

    clean code

--- app/src/main/java/fr/eanathos/antsidentifier/MainActivity.java
+++ app/src/main/java/fr/eanathos/antsidentifier/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.eanathos.antsidentifier;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+    }
 
+    public void initView() {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -27,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -37,5 +40,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
